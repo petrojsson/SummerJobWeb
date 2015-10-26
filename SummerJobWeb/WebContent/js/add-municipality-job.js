23119993f6f0c16a23f41494b0b1e16232d55b38
Petter Wallin <wallin.petter@gmail.com>
2015-10-26 11:37:41 +0100

    js, css etc

---
+++ SummerJobWeb/WebContent/js/add-municipality-job.js
@@ -0,0 +1,13 @@
+/**
+ * New node file
+ */
+
+
+$(document).ready(function(){
+	if($('.add-mentor-btn')){		
+		$('.add-mentor-btn').click(function(e){
+			e.preventDefault();
+			$('#mentors-wrapper').append($('#mentor-template').html());			
+		});
+	}	
+});
